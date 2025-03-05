78fac9c21304c0ee16f923655b7cce008ff950e7
iThinkAle <alessandrorico@outlook.it>
2025-03-06 00:05:08 +0100

    Commit

---
+++ informatica/ricordy/es_04-03-25.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool numprimo(int a){
+	if(a <= 1){
+		return false;
+	}
+	else{
+		for(int i = 2; i <= a / 2; i++){
+			if(a % i == 0){
+				return false;
+				break;
+			}
+		}
+	}
+	return true;
+}
+
+
+int main(){
+	int num;
+	
+	cout << "inserisci numero" << endl;
+	cin >> num;
+	cout << "il numero è primo? " << boolalpha << numprimo(num) << endl;
+	
+	return 0;
+}
